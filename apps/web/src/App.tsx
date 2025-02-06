5dba5ebc8fec68dcda98735fe215f8f128f7f57b
minchodang <minsu910725@gmail.com>
2025-02-06 13:43:10 +0900

    feat: client setting

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<h1>Hello from React App!</h1>
+			<p>This is a sample component.</p>
+		</div>
+	);
+};
+
+export default App;

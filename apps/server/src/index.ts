e35240d3d0d396bb8e2e9a6b64ec488729d9a1a1
minchodang <minsu910725@gmail.com>
2025-02-07 16:50:43 +0900

    feat: plugins 설정 및 cors

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv-safe/config.js";
 import { makeApp } from "./makeApp";
 
 const HOST = process.env.HOST ?? "0.0.0.0";
@@ -9,3 +10,5 @@ await app.listen({
 	host: HOST,
 	port: PORT,
 });
+
+app.log.info({}, `Server listening at http://localhost:${PORT}`);

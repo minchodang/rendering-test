9b9dfaf8e0f292940c6dd6ca3b98f00f987dca82
minchodang <minsu910725@gmail.com>
2025-02-22 19:59:18 +0900

    feat: gql data loader

---
+++ apps/server/src/plugins/req.dataloaders.ts
@@ -0,0 +1,23 @@
+import fp from "fastify-plugin";
+
+declare module "fastify" {
+	interface FastifyRequest {
+		dataloaders: {
+			a: 3;
+		};
+	}
+}
+
+export default fp(
+	(app) => {
+		app.decorateRequest("dataloaders", {});
+		app.addHook("preHandler", async (req) => {
+			req.dataloaders = {
+				a: 3,
+			};
+		});
+	},
+	{
+		name: "req.dataloaders",
+	},
+);

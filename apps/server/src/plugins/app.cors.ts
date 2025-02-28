b5efd8ab75f1ea486bc19a6f316d0d0ec4fb7e8d
minchodang <minsu910725@gmail.com>
2025-02-28 22:30:03 +0900

    feat: relay runtime setting

--- apps/server/src/plugins/app.cors.ts
+++ apps/server/src/plugins/app.cors.ts
@@ -1,6 +1,5 @@
 import fastifyCors from "@fastify/cors";
 import fp from "fastify-plugin";
-import s from '../../../web/src/__relay__'
 export default fp(
 	async (app) => {
 		await app.register(fastifyCors, {

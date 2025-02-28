53a762759e4a827a212a60e5bb804dd303aa2bc0
minchodang <minsu910725@gmail.com>
2025-02-28 22:30:03 +0900

    feat: relay runtime setting

---
+++ apps/web/src/relay/createRelayEnvironment.ts
@@ -0,0 +1,24 @@
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+
+const network = Network.create(async (operation, variables, cacheConfig) => {
+  const response = await fetch("http://127.0.0.1:5173/graphql", {
+    method: "post",
+    body: JSON.stringify({
+      query: operation?.text,
+      variables,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    signal: cacheConfig.metadata?.signal as AbortSignal,
+  });
+
+  return await response.json();
+});
+
+const store = new Store(new RecordSource());
+
+const relayEnvironment = new Environment({
+  network,
+  store,
+});
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import relay from "vite-plugin-relay-lite";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), relay()],
 });
